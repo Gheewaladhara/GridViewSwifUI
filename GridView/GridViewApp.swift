fe119526d8b6f209a5d73718739a479352cd1fe7
dharagheewala <dharagheewalaa@gmail.com>
2024-05-24 19:55:24 +0530

    Initial Commit

---
+++ GridView/GridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewApp.swift
+//  GridView
+//
+//  Created by GHEEWALA DHARA on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
